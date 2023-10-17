06780b7ae702c8be0a939040a8e340ca50c88dc0
JiEun Park <53637261+rioju412@users.noreply.github.com>
2023-10-17 10:59:23 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -421,11 +421,11 @@ class DetectionPredictor(BasePredictor):
 
 
 @hydra.main(version_base=None, config_path=str(DEFAULT_CONFIG.parent), config_name=DEFAULT_CONFIG.name)
-def predict(cfg, model_name="bestv2.pt", source_name="0", show=True):
+def predict(cfg):
     init_tracker()
-    cfg.model = model_name
-    cfg.source = source_name
-    cfg.show = show
+    cfg.model = cfg.model or "bestv2.pt"
+    cfg.imgsz = check_imgsz(cfg.imgsz, min_dim=2)  # check image size
+    cfg.source = cfg.source if cfg.source is not None else ROOT / "assets"
     predictor = DetectionPredictor(cfg)
     predictor()
 
